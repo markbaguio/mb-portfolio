88613f53d39ce29adb7e604358d5516050784601
Mark Godwin C. Baguio <godwinbaguio@gmail.com>
2026-01-21 16:10:09 +0800

    chore: change button hover state color.

--- src/App.tsx
+++ src/App.tsx
@@ -45,7 +45,7 @@ function App() {
               <span className="font-light flex justify-center items-center gap-1 text-sm">
                 <MapPinHouse size={12} /> Bulacan, Philippines
               </span>
-              <button className="bg-primary-navy-indigo rounded-sm p-2 flex gap-1 text-sm hover:cursor-pointer hover:text-background-graphene-black hover:bg-text-soft-lavender-white  ease-in-out duration-500 hover:scale-110">
+              <button className="bg-primary-navy-indigo rounded-sm p-2 flex gap-1 text-sm hover:cursor-pointer  hover:bg-primary-navy-indigo/50  ease-in-out duration-500 hover:scale-110">
                 <Download size={16} /> Resume
               </button>
             </div>
@@ -76,29 +76,6 @@ function App() {
         {/** right column */}
         <div className="col-span-1 p-10 my-20 overflow-y-auto bg-text-soft-lavender-white/3 border border-secondary-iron-charcoal shadow-sm shadow-secondary-iron-charcoal drop-shadow-2xl rounded-2xl">
           <AboutMe />
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, fuga?
-          Eaque, natus. Nostrum, sint rem nesciunt atque pariatur nobis iste ex
-          voluptate deserunt ipsa voluptatum, natus consectetur provident.
-          Accusamus, vel distinctio architecto laudantium quod cumque minus
-          assumenda, aspernatur reprehenderit nisi nostrum illo! Iure dicta
-          optio expedita, sint delectus iusto possimus enim, perspiciatis cum
-          rerum vero harum excepturi earum illo quis! Vero amet doloremque,
-          deleniti dolorum provident quasi veniam nobis aliquid quia ut itaque
-          fugit et officia nisi dolores enim omnis sit sapiente, maiores
-          perspiciatis. Suscipit minima harum voluptatem laudantium quidem porro
-          blanditiis recusandae eligendi nobis, optio illo fugit laborum neque
-          eaque reprehenderit expedita. Nostrum impedit ipsum earum sequi
-          ratione sapiente, commodi expedita! Soluta doloremque aut eaque
-          obcaecati quisquam ullam voluptatem sint quibusdam deserunt ut magni
-          impedit, delectus accusantium voluptate. Minima praesentium accusamus
-          molestias odit quibusdam nihil atque, dolorum deleniti perferendis
-          modi ut natus cupiditate exercitationem, architecto culpa quasi,
-          doloribus iusto nisi! Illum, cupiditate laudantium ex tenetur dolores
-          quidem incidunt vitae voluptates explicabo qui minus, ipsum pariatur
-          suscipit voluptate nesciunt sed labore. Nulla velit perferendis
-          tempora laboriosam esse? Eligendi doloribus earum molestiae atque sunt
-          dolorum quia animi, at praesentium eos nesciunt id neque corrupti,
-          incidunt, unde a distinctio nostrum perspiciatis culpa!
         </div>
       </div>
     </main>
