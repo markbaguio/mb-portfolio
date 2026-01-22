11155dcfdd15615e3006a4b62d7e23d182eb9188
Mark Godwin C. Baguio <godwinbaguio@gmail.com>
2026-01-22 15:50:42 +0800

    wip: add logo wip: add experience section.

--- src/lib/Experience.tsx
+++ src/lib/Experience.tsx
@@ -5,43 +5,62 @@ function Experience() {
         <h2 className="text-3xl font-semibold w-full border-b-6 border-primary-navy-indigo mb-5">
           Experience
         </h2>
-        <div className=" border-l-2 border-accent-graphite-blue px-3">
-          <h4 className="text-lg font-semibold pb-1">
-            Associate Software Engineer
-          </h4>
-          <p className="text-sm">Amici Mercantile Inc.</p>
-          <p className="text-sm">Quezon City, Metro Manila</p>
-          <p className="text-sm font-extralight pb-5">2023 - 2024</p>
-          <ul className="list-disc pl-10 text-sm leading-7">
-            <li>
-              Contributed to the redevelopment of a legacy POS/enterprise system
-              written in Delphi, transitioning it to a modern, cross-platform
-              architecture using Flutter. Worked on both front end and back end.
-            </li>
-            <li>
-              Implemented new features requested by external clients and
-              internal stakeholders, improving system functionality and meeting
-              diverse user needs.
-            </li>
-            <li>
-              Provided technical support, addressing and resolving bugs reported
-              through a ticketing system, ensuring seamless client operations
-              and satisfaction.
-            </li>
-            <li>
-              Developed the front end of various Flutter applications,
-              including:
-              <ul className="list-disc pl-10">
-                <li>A Bundy clock app originally built in java.</li>
-                <li>
-                  An in-house application that allows agents to input pool
-                  dimensions and automatically generates product recommendations
-                  (e.g., pumps, filters) based on calculated specifications,
-                  streamlining the sales and quoting process.
-                </li>
-              </ul>
-            </li>
-          </ul>
+        <div className="flex flex-col gap-5 border-l-2 border-accent-graphite-blue px-3">
+          <div>
+            <h4 className="text-lg font-semibold pb-1">
+              Associate Software Engineer
+            </h4>
+            <p className="text-sm">Amici Mercantile Inc.</p>
+            <p className="text-sm">Quezon City, Metro Manila</p>
+            <p className="text-sm font-extralight pb-5">2023 - 2024</p>
+            <ul className="list-disc pl-10 text-sm leading-7">
+              <li>
+                Contributed to the redevelopment of a legacy POS/enterprise
+                system written in Delphi, transitioning it to a modern,
+                cross-platform architecture using Flutter. Worked on both front
+                end and back end.
+              </li>
+              <li>
+                Implemented new features requested by external clients and
+                internal stakeholders, improving system functionality and
+                meeting diverse user needs.
+              </li>
+              <li>
+                Provided technical support, addressing and resolving bugs
+                reported through a ticketing system, ensuring seamless client
+                operations and satisfaction.
+              </li>
+              <li>
+                Developed the front end of various Flutter applications,
+                including:
+                <ul className="list-disc pl-10">
+                  <li>A Bundy clock app originally built in java.</li>
+                  <li>
+                    An in-house application that allows agents to input pool
+                    dimensions and automatically generates product
+                    recommendations (e.g., pumps, filters) based on calculated
+                    specifications, streamlining the sales and quoting process.
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h4 className="text-lg font-semibold pb-1">
+              Clound Engineer Intern
+            </h4>
+            <p className="text-sm">
+              Institute of Computer Engineers of the Philippines
+            </p>
+            <p className="text-sm">Singapore</p>
+            <p className="text-sm font-extralight pb-5">2021</p>
+            <ul className="list-disc pl-10 text-sm leading-7">
+              <li>
+                Created and deployed a WordPress website using a LAMP stack
+                (Linux, Apache, MySQL, PHP) on an AWS EC2 instance.
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
       <div>
